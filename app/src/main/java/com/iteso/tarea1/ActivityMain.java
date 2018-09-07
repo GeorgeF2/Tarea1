dd248b0b597f8aaf18f6e908d2f2527aa4bf463d
Jorge Perez <alberto.perezf2@gmail.com>
2018-09-06 23:53:56 -0500

    Finished spinner

--- app/src/main/java/com/iteso/tarea1/ActivityMain.java
+++ app/src/main/java/com/iteso/tarea1/ActivityMain.java
@@ -2,12 +2,24 @@ package com.iteso.tarea1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.activity_main_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.escolaridad, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
