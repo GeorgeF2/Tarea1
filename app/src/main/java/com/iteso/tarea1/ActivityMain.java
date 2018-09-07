1d9da4c2e735a28d952171dd80b3b21f0e0a93c0
Jorge Perez <alberto.perezf2@gmail.com>
2018-09-07 00:24:32 -0500

    Finished autocomplete text

--- app/src/main/java/com/iteso/tarea1/ActivityMain.java
+++ app/src/main/java/com/iteso/tarea1/ActivityMain.java
@@ -3,11 +3,13 @@ package com.iteso.tarea1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 
 public class ActivityMain extends AppCompatActivity {
 
     Spinner spinner;
+    AutoCompleteTextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,15 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinner = findViewById(R.id.activity_main_spinner);
+        textView = findViewById(R.id.activity_main_auto);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.escolaridad, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        String[] books = getResources().getStringArray(R.array.libros);
+        ArrayAdapter<String> adapterA = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, books);
+        textView.setAdapter(adapterA);
 
 
     }
